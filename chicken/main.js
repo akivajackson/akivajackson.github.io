24d4ee422017f2cb83317886306de6401092a1ec
akivajackson <akivajackson@users.noreply.github.com>
2024-01-16 14:14:05 +0000

    deploy: 5d9620a7796557393f9563187aacaf0b18646b97

--- chicken/main.js
+++ chicken/main.js
@@ -18,22 +18,26 @@ let songs = [
     'songs/poppy-chicken-symphony.mp3',
 ]
 let gifs = [
-    'gifs/sunset.gif',
     'gifs/dj.gif',
+    'gifs/metal.gif',
     'gifs/indie.gif',
     'gifs/DALL·E-2024-01-15-18.53.06---A-chicken-on-a-raft-in-a-warm-and-rustic-folk-style.-The-chicken-is-peaceful-and-serene,-depicted-with-natural-and-earthy-tones.-The-raft-is-made-of-n.png',
-    'gifs/metal.gif',
+    'gifs/DALL·E-2024-01-15-21.57.41---A-chicken-on-a-raft-in-a-Constructivist-art-style.-The-image-is-bold-and-geometric,-with-an-emphasis-on-abstract-forms-and-industrial-materials.-The-c.png',
     'gifs/DALL·E-2024-01-15-18.48.02---A-jazz-album-cover-featuring-a-chicken-on-a-raft.-The-style-is-cool-and-sophisticated,-with-a-hint-of-vintage-charm.-The-chicken-is-stylized-in-a-slee.png',
     'gifs/DALL·E-2024-01-15-18.53.16---A-chicken-on-a-raft-in-a-grand-and-dramatic-opera-style.-The-chicken-is-depicted-in-a-theatrical-and-powerful-manner,-possibly-wearing-a-miniature-ope.png',
+    'gifs/sunset.gif',
+    'gifs/DALL·E-2024-01-15-21.56.18---A-chicken-on-a-raft-in-a-surrealistic-style-inspired-by-Salvador-Dali.-The-image-features-dream-like-elements-with-the-chicken-and-raft-melting-and-di.png',
     'gifs/DALL·E-2024-01-15-18.42.49---An-album-cover-for-a-death-metal-song-featuring-a-chicken-on-a-raft.-The-style-is-dark-and-intense,-with-the-chicken-portrayed-in-a-fierce,-almost-men.png',
-    'gifs/DALL·E-2024-01-15-18.47.57---An-opera-album-cover-with-a-chicken-on-a-raft.-The-style-is-grand-and-dramatic,-reflecting-the-theatrical-nature-of-opera.-The-chicken-is-depicted-in-.png',
+    'gifs/DALL·E-2024-01-15-21.56.24---A-chicken-on-a-raft-in-a-contemporary-street-photography-style.-The-image-captures-a-candid,-spontaneous-moment-of-a-real-chicken-on-a-small-raft,-per.png',
+    'gifs/DALL·E-2024-01-15-21.56.27---A-chicken-on-a-raft-in-a-traditional-Chinese-ink-wash-painting-style.-The-image-is-elegant-and-minimalistic,-with-fluid-brushstrokes-and-a-focus-on-th.png',
     'gifs/DALL·E-2024-01-15-18.48.08---A-folk-album-cover-featuring-a-chicken-on-a-raft.-The-style-is-warm-and-rustic,-with-the-chicken-depicted-in-a-peaceful,-serene-manner.-The-raft-is-ma.png',
-    'gifs/DALL·E-2024-01-15-18.53.21---A-chicken-on-a-raft-in-a-cool-and-sophisticated-jazz-style.-The-chicken-is-stylized-in-a-sleek,-jazzy-manner,-possibly-with-elements-like-a-hat-or-a-s.png',
+    'gifs/DALL·E-2024-01-15-18.47.57---An-opera-album-cover-with-a-chicken-on-a-raft.-The-style-is-grand-and-dramatic,-reflecting-the-theatrical-nature-of-opera.-The-chicken-is-depicted-in-.png',
     'gifs/DALL·E-2024-01-15-21.46.46---A-chicken-on-a-raft-in-a-fantasy-art-style,-reminiscent-of-a-scene-from-a-fairy-tale.-The-chicken-appears-mystical-and-enchanting,-possibly-with-magic.png',
     'gifs/DALL·E-2024-01-15-21.46.48---A-chicken-on-a-raft-in-a-traditional-Japanese-ukiyo-e-woodblock-print-style.-The-image-features-classic-ukiyo-e-elements,-such-as-curving-lines-and-fl.png',
     'gifs/DALL·E-2024-01-15-21.46.50---A-chicken-on-a-raft-in-a-minimalist,-modern-art-style.-The-image-is-composed-with-simple,-clean-lines-and-a-limited-color-palette.-The-chicken-is-styl.png',
     'gifs/DALL·E-2024-01-15-21.46.52---A-chicken-on-a-raft-in-a-futuristic,-cyberpunk-style.-The-chicken-is-enhanced-with-neon-and-cybernetic-elements,-giving-it-a-bold,-edgy-look.-The-raft.png',
     'gifs/DALL·E-2024-01-15-21.46.54---A-chicken-on-a-raft-in-an-impressionist-painting-style.-The-image-features-soft-brushstrokes-and-a-focus-on-the-play-of-light-and-color.-The-chicken-i.png',
+    'gifs/DALL·E-2024-01-15-18.53.21---A-chicken-on-a-raft-in-a-cool-and-sophisticated-jazz-style.-The-chicken-is-stylized-in-a-sleek,-jazzy-manner,-possibly-with-elements-like-a-hat-or-a-s.png',
     'gifs/DALL·E-2024-01-15-21.51.14---A-chicken-on-a-raft-in-a-Gothic-art-style.-The-image-is-dark-and-moody,-with-dramatic-lighting-and-shadow-play.-The-chicken-is-depicted-in-a-mysteriou.png',
     'gifs/DALL·E-2024-01-15-21.51.16---A-chicken-on-a-raft-in-a-pop-art-style.-The-image-features-bold-colors,-stark-outlines,-and-a-playful,-whimsical-approach.-The-chicken-is-depicted-in-.png',
     'gifs/DALL·E-2024-01-15-21.51.18---A-chicken-on-a-raft-in-an-Art-Deco-style.-The-image-is-characterized-by-elegant,-geometric-shapes-and-streamlined-forms.-The-chicken-is-stylized-with-.png',
@@ -44,13 +48,9 @@ let gifs = [
     'gifs/DALL·E-2024-01-15-21.56.12---A-chicken-on-a-raft-in-a-Baroque-painting-style.-The-image-is-rich-and-dramatic,-with-deep-shadows-and-intense-lighting.-The-chicken-is-depicted-with-.png',
     'gifs/DALL·E-2024-01-15-21.56.14---A-chicken-on-a-raft-in-a-digital-pixel-art-style.-The-image-is-composed-of-vibrant-pixels,-creating-a-nostalgic-and-playful-scene.-The-chicken-is-depi.png',
     'gifs/DALL·E-2024-01-15-21.56.15---A-chicken-on-a-raft-in-a-street-art-graffiti-style.-The-image-is-vibrant-and-expressive,-with-bold-spray-paint-effects-and-urban-influences.-The-chick.png',
-    'gifs/DALL·E-2024-01-15-21.56.18---A-chicken-on-a-raft-in-a-surrealistic-style-inspired-by-Salvador-Dali.-The-image-features-dream-like-elements-with-the-chicken-and-raft-melting-and-di.png',
     'gifs/DALL·E-2024-01-15-21.56.20---A-chicken-on-a-raft-in-a-Northern-Renaissance-style.-The-image-is-detailed-and-intricate,-with-an-emphasis-on-realism-and-fine-textures.-The-chicken-i.png',
     'gifs/DALL·E-2024-01-15-21.56.22---A-chicken-on-a-raft-in-an-Expressionist-style.-The-image-is-characterized-by-intense,-vivid-colors-and-bold,-distorted-forms,-conveying-emotional-expe.png',
-    'gifs/DALL·E-2024-01-15-21.56.24---A-chicken-on-a-raft-in-a-contemporary-street-photography-style.-The-image-captures-a-candid,-spontaneous-moment-of-a-real-chicken-on-a-small-raft,-per.png',
-    'gifs/DALL·E-2024-01-15-21.56.27---A-chicken-on-a-raft-in-a-traditional-Chinese-ink-wash-painting-style.-The-image-is-elegant-and-minimalistic,-with-fluid-brushstrokes-and-a-focus-on-th.png',
     'gifs/DALL·E-2024-01-15-21.57.39---A-chicken-on-a-raft-in-a-Rococo-art-style.-The-image-is-light,-playful,-and-ornate,-with-delicate-colors-and-intricate-details.-The-chicken-is-adorned.png',
-    'gifs/DALL·E-2024-01-15-21.57.41---A-chicken-on-a-raft-in-a-Constructivist-art-style.-The-image-is-bold-and-geometric,-with-an-emphasis-on-abstract-forms-and-industrial-materials.-The-c.png',
     'gifs/DALL·E-2024-01-15-21.57.43---A-chicken-on-a-raft-in-a-Pre-Raphaelite-art-style.-The-image-is-rich-in-symbolism-and-natural-detail,-with-an-emphasis-on-beauty-and-emotion.-The-chic.png',
     'gifs/DALL·E-2024-01-15-21.58.45---A-chicken-on-a-raft-in-a-contemporary-conceptual-art-style.-The-image-challenges-traditional-perceptions-with-an-abstract-and-thought-provoking-approa.png',
     'gifs/DALL·E-2024-01-15-21.59.31---A-chicken-on-a-raft-in-a-Neo-Expressionist-style.-The-image-is-characterized-by-intense-emotion-and-raw-energy,-with-vigorous-brushwork-and-vivid-colo.png',
@@ -61,48 +61,61 @@ let currentGifIndex = 0;
 let audio = document.getElementById('audio');
 let image = document.getElementById('startImage');
 
+nextSong = function () {
+    currentSongIndex = (currentSongIndex + 1) % songs.length;
+    audio.src = songs[currentSongIndex];
+    audio.play();
+
+    currentGifIndex = (currentGifIndex + 1) % gifs.length;
+    image.src = gifs[currentGifIndex];
+    for (let li of lis) {
+        li.classList.remove('current');
+    }
+    lis[currentGifIndex + 1].classList.add('current');
+    lis[currentGifIndex + 1].scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'center'});
+};
+
+// Add the carousel of images
+let carousel = document.getElementById('carousel');
+let ul = document.getElementById('carouselList');
+// Add an empty li at the start
+let startLi = document.createElement('li');
+startLi.classList.add('padding');
+ul.appendChild(startLi);
+for (let i = 0; i < gifs.length; i++) {
+    let li = document.createElement('li');
+    let img = document.createElement('img');
+    img.src = gifs[i];
+    img.onclick = function () {
+        currentGifIndex = i - 1;
+        currentSongIndex = i - 1;
+        nextSong();
+    };
+    li.appendChild(img);
+    ul.appendChild(li);
+}
+// Add an empty li at the end
+let endLi = document.createElement('li');
+endLi.classList.add('padding');
+let lis = document.querySelectorAll('#carousel li');
+ul.appendChild(endLi);
+carousel.appendChild(ul);
+
 function startVideo() {
     image.setAttribute('src', gifs[currentGifIndex]);
     image.setAttribute('id', 'gif');
     audio.play();
+    lis[currentGifIndex + 1].classList.add('current');
 
-    image.onclick = function () {
-        currentSongIndex++;
-        if (currentSongIndex >= songs.length) {
-            currentSongIndex = 0;
-        }
-        audio.src = songs[currentSongIndex];
-        audio.play();
-
-        currentGifIndex++;
-        if (currentGifIndex >= gifs.length) {
-            currentGifIndex = 0;
-        }
-        gif.src = gifs[currentGifIndex];
-    };
-
+    image.onclick = nextSong;
 
     document.getElementById('counter-container').style.display = 'block';
-    let clickBack = document.getElementById('click-back')
-    clickBack.style.display = 'block';
 
     interval = setInterval(function () {
         count++;
         counter.innerText = count;
     }, 1000);
-}
 
-function backOne() {
-    currentSongIndex--;
-    if (currentSongIndex < 0) {
-        currentSongIndex = songs.length - 1;
-    }
-    audio.src = songs[currentSongIndex];
-    audio.play();
-
-    currentGifIndex--;
-    if (currentGifIndex < 0) {
-        currentGifIndex = gifs.length - 1;
-    }
-    image.src = gifs[currentGifIndex];
+    // Make the carousel visible
+    carousel.style.display = 'block';
 }
