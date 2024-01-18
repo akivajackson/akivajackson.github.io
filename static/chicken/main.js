53de756311679afb5ab424cf0e6b865d6b750d63
akivajackson <106350560+akivajackson@users.noreply.github.com>
2024-01-18 11:38:41 -0500

    ai reordering of images

--- static/chicken/main.js
+++ static/chicken/main.js
@@ -45,39 +45,39 @@ let songsAndGifs = [
     }, {
         "genre": "Club Nightlife",
         "song": 'club—Dance-All-Night.mp3',
-        "gif": 'DALL·E-2024-01-15-21.57.39---A-chicken-on-a-raft-in-a-Rococo-art-style.-The-image-is-light,-playful,-and-ornate,-with-delicate-colors-and-intricate-details.-The-chicken-is-adorned.png',
+        "gif": 'DALL·E-2024-01-15-21.51.18---A-chicken-on-a-raft-in-an-Art-Deco-style.-The-image-is-characterized-by-elegant,-geometric-shapes-and-streamlined-forms.-The-chicken-is-stylized-with-.png'
     }, {
         "genre": "Funk Fusion",
         "song": "funk-groovy--Funky-Chicken.mp3",
-        "gif": 'DALL·E-2024-01-15-21.56.27---A-chicken-on-a-raft-in-a-traditional-Chinese-ink-wash-painting-style.-The-image-is-elegant-and-minimalistic,-with-fluid-brushstrokes-and-a-focus-on-th.png',
+        "gif": 'DALL·E-2024-01-15-21.56.20---A-chicken-on-a-raft-in-a-Northern-Renaissance-style.-The-image-is-detailed-and-intricate,-with-an-emphasis-on-realism-and-fine-textures.-The-chicken-i.png'
     }, {
         "genre": "Country Metal Fusion",
         "song": 'country-Chicken-on-a-Raft-(2)-heavy-metal-hard-hitting-intense.mp3',
-        "gif": 'DALL·E-2024-01-15-21.46.48---A-chicken-on-a-raft-in-a-traditional-Japanese-ukiyo-e-woodblock-print-style.-The-image-features-classic-ukiyo-e-elements,-such-as-curving-lines-and-fl.png',
+        "gif": 'DALL·E-2024-01-15-18.42.49---An-album-cover-for-a-death-metal-song-featuring-a-chicken-on-a-raft.-The-style-is-dark-and-intense,-with-the-chicken-portrayed-in-a-fierce,-almost-men.png'
     }, {
         "genre": "Seafarer's Tune",
         "song": 'Skippers-Lament.mp3',
-        "gif": 'DALL·E-2024-01-15-21.57.43---A-chicken-on-a-raft-in-a-Pre-Raphaelite-art-style.-The-image-is-rich-in-symbolism-and-natural-detail,-with-an-emphasis-on-beauty-and-emotion.-The-chic.png',
+        "gif": 'DALL·E-2024-01-15-21.57.39---A-chicken-on-a-raft-in-a-Rococo-art-style.-The-image-is-light,-playful,-and-ornate,-with-delicate-colors-and-intricate-details.-The-chicken-is-adorned.png'
     }, {
         "genre": "(Westernized) Chinese Folklore",
         "song": "folk-traditional-chinese-enchanting-The-Adventure-of-the-Chicken-on-the-Raft.mp3",
-        "gif": 'DALL·E-2024-01-15-21.56.15---A-chicken-on-a-raft-in-a-street-art-graffiti-style.-The-image-is-vibrant-and-expressive,-with-bold-spray-paint-effects-and-urban-influences.-The-chick.png',
+        "gif": 'DALL·E-2024-01-15-21.56.27---A-chicken-on-a-raft-in-a-traditional-Chinese-ink-wash-painting-style.-The-image-is-elegant-and-minimalistic,-with-fluid-brushstrokes-and-a-focus-on-th.png'
     }, {
         "genre": "Indian Classical",
         "song": "traditional-indian-sitar-tabla-The-Rafting-Chicken.mp3",
-        "gif": 'DALL·E-2024-01-15-21.57.41---A-chicken-on-a-raft-in-a-Constructivist-art-style.-The-image-is-bold-and-geometric,-with-an-emphasis-on-abstract-forms-and-industrial-materials.-The-c.png',
+        "gif": 'DALL·E-2024-01-15-21.56.09---A-chicken-on-a-raft-in-a-Victorian-era-illustration-style.-The-image-has-a-classic,-vintage-feel,-with-detailed-line-work-and-a-monochromatic-color-sc.png'
     }, {
         "genre": "Hallelujah",
         "song": 'chicken-hallelujah.mp3',
-        "gif": 'DALL·E-2024-01-15-21.56.18---A-chicken-on-a-raft-in-a-surrealistic-style-inspired-by-Salvador-Dali.-The-image-features-dream-like-elements-with-the-chicken-and-raft-melting-and-di.png',
+        "gif": 'DALL·E-2024-01-15-21.57.41---A-chicken-on-a-raft-in-a-Constructivist-art-style.-The-image-is-bold-and-geometric,-with-an-emphasis-on-abstract-forms-and-industrial-materials.-The-c.png'
     }, {
         "genre": "Opera Drama",
         "song": 'Chicken-on-a-Raft-(Opera-Edition).mp3',
-        "gif": 'DALL·E-2024-01-15-18.42.49---An-album-cover-for-a-death-metal-song-featuring-a-chicken-on-a-raft.-The-style-is-dark-and-intense,-with-the-chicken-portrayed-in-a-fierce,-almost-men.png',
+        "gif": 'DALL·E-2024-01-15-18.53.16---A-chicken-on-a-raft-in-a-grand-and-dramatic-opera-style.-The-chicken-is-depicted-in-a-theatrical-and-powerful-manner,-possibly-wearing-a-miniature-ope.png'
     }, {
         "genre": "Melodramatic Waves",
         "song": 'melodrama—chicken-sailors-ode.mp3',
-        "gif": 'DALL·E-2024-01-15-18.53.16---A-chicken-on-a-raft-in-a-grand-and-dramatic-opera-style.-The-chicken-is-depicted-in-a-theatrical-and-powerful-manner,-possibly-wearing-a-miniature-ope.png',
+        "gif": 'DALL·E-2024-01-15-21.56.18---A-chicken-on-a-raft-in-a-surrealistic-style-inspired-by-Salvador-Dali.-The-image-features-dream-like-elements-with-the-chicken-and-raft-melting-and-di.png'
     }, {
         "genre": "Pop Symphony",
         "song": 'poppy-chicken-symphony.mp3',
@@ -89,19 +89,19 @@ let songsAndGifs = [
     }, {
         "genre": "Arabesque Rhythm",
         "song": "lively-traditional-arabesque--Raftin'-Chicken.mp3",
-        "gif": 'DALL·E-2024-01-15-18.53.21---A-chicken-on-a-raft-in-a-cool-and-sophisticated-jazz-style.-The-chicken-is-stylized-in-a-sleek,-jazzy-manner,-possibly-with-elements-like-a-hat-or-a-s.png',
+        "gif": 'DALL·E-2024-01-15-21.51.14---A-chicken-on-a-raft-in-a-Gothic-art-style.-The-image-is-dark-and-moody,-with-dramatic-lighting-and-shadow-play.-The-chicken-is-depicted-in-a-mysteriou.png'
     }, {
         "genre": "Medieval Echoes",
         "song": "medieval-choral-gregorianChanting-Chicken.mp3",
-        "gif": 'DALL·E-2024-01-15-21.51.14---A-chicken-on-a-raft-in-a-Gothic-art-style.-The-image-is-dark-and-moody,-with-dramatic-lighting-and-shadow-play.-The-chicken-is-depicted-in-a-mysteriou.png',
+        "gif": 'DALL·E-2024-01-15-21.51.16---A-chicken-on-a-raft-in-a-pop-art-style.-The-image-features-bold-colors,-stark-outlines,-and-a-playful,-whimsical-approach.-The-chicken-is-depicted-in-.png'
     }, {
         "genre": "Maqam Folk",
         "song": "traditional-maqam-folk-The-Majestic-Chicken.mp3",
-        "gif": 'DALL·E-2024-01-15-21.51.16---A-chicken-on-a-raft-in-a-pop-art-style.-The-image-features-bold-colors,-stark-outlines,-and-a-playful,-whimsical-approach.-The-chicken-is-depicted-in-.png',
+        "gif": 'DALL·E-2024-01-15-18.53.21---A-chicken-on-a-raft-in-a-cool-and-sophisticated-jazz-style.-The-chicken-is-stylized-in-a-sleek,-jazzy-manner,-possibly-with-elements-like-a-hat-or-a-s.png'
     }, {
         "genre": "Upbeat Chinese-ish Folk",
         "song": "upbeat-traditional-chinese-folk-Chicken-on-a-Raft.mp3",
-        "gif": 'DALL·E-2024-01-15-21.51.18---A-chicken-on-a-raft-in-an-Art-Deco-style.-The-image-is-characterized-by-elegant,-geometric-shapes-and-streamlined-forms.-The-chicken-is-stylized-with-.png',
+        "gif": 'DALL·E-2024-01-15-21.57.43---A-chicken-on-a-raft-in-a-Pre-Raphaelite-art-style.-The-image-is-rich-in-symbolism-and-natural-detail,-with-an-emphasis-on-beauty-and-emotion.-The-chic.png'
     }, {
         "genre": "Balkan Isopolyphony",
         "song": "-balkan-isopolyphony-traditionalThe-Raft-of-the-Chicken.mp3",
@@ -109,7 +109,7 @@ let songsAndGifs = [
     }, {
         "genre": "Choral Classics",
         "song": "-choral-classical-Cluck-Cluck-Harmony.mp3",
-        "gif": 'DALL·E-2024-01-15-21.56.09---A-chicken-on-a-raft-in-a-Victorian-era-illustration-style.-The-image-has-a-classic,-vintage-feel,-with-detailed-line-work-and-a-monochromatic-color-sc.png',
+        "gif": 'DALL·E-2024-01-15-21.46.48---A-chicken-on-a-raft-in-a-traditional-Japanese-ukiyo-e-woodblock-print-style.-The-image-features-classic-ukiyo-e-elements,-such-as-curving-lines-and-fl.png'
     }, {
         "genre": "Gregorian Solemnity",
         "song": "-solemn-gregorian-chants-reverentFeathered-Voyage.mp3",
@@ -117,11 +117,11 @@ let songsAndGifs = [
     }, {
         "genre": "Worldly Beats",
         "song": "-traditional-world-upbeat-Raftin'-Chicken.mp3",
-        "gif": 'DALL·E-2024-01-15-21.56.20---A-chicken-on-a-raft-in-a-Northern-Renaissance-style.-The-image-is-detailed-and-intricate,-with-an-emphasis-on-realism-and-fine-textures.-The-chicken-i.png',
+        "gif": 'DALL·E-2024-01-15-21.56.22---A-chicken-on-a-raft-in-an-Expressionist-style.-The-image-is-characterized-by-intense,-vivid-colors-and-bold,-distorted-forms,-conveying-emotional-expe.png'
     }, {
         "genre": "Fusion Excitement",
         "song": "-upbeat-fusion-world-Raft-of-the-Rooster.mp3",
-        "gif": 'DALL·E-2024-01-15-21.56.22---A-chicken-on-a-raft-in-an-Expressionist-style.-The-image-is-characterized-by-intense,-vivid-colors-and-bold,-distorted-forms,-conveying-emotional-expe.png',
+        "gif": 'DALL·E-2024-01-15-21.56.15---A-chicken-on-a-raft-in-a-street-art-graffiti-style.-The-image-is-vibrant-and-expressive,-with-bold-spray-paint-effects-and-urban-influences.-The-chick.png',
     },
 ].map(songAndGif => {
     songAndGif.song = "songs/" + songAndGif.song;
@@ -148,7 +148,7 @@ nextSong = function (increment = 1) {
     }
     lis[currentIndex + 1].classList.add('current');
     lis[currentIndex + 1].scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'center'});
-    
+
     //preload the next song
     let nextSongIndex = (currentIndex + 1) % songsAndGifs.length;
     let nextSong = new Audio();
@@ -205,7 +205,7 @@ function startVideo() {
 let first_img = new Image();
 first_img.src = songsAndGifs[0].gif;
 
-window.addEventListener('keydown', function(event) {
+window.addEventListener('keydown', function (event) {
     if (event.key === 'ArrowRight') {
         nextSong(1);
     } else if (event.key === 'ArrowLeft') {
@@ -213,7 +213,7 @@ window.addEventListener('keydown', function(event) {
     }
 });
 
-document.getElementById('muteButton').addEventListener('click', function() {
+document.getElementById('muteButton').addEventListener('click', function () {
     let audioElem = document.getElementById('audio');
     if (audioElem.muted) {
         // If the audio is currently muted, unmute it and change the image source
@@ -226,8 +226,8 @@ document.getElementById('muteButton').addEventListener('click', function() {
     }
 });
 
-document.addEventListener("visibilitychange", function() {
-    if (document.hidden){
+document.addEventListener("visibilitychange", function () {
+    if (document.hidden) {
         audio.pause();
     } else {
         audio.play();
