be7f78c1949d0248ff73a935ba29f2f00eb20ab8
akivajackson <106350560+akivajackson@users.noreply.github.com>
2024-01-18 10:27:38 -0500

    add objects

--- static/chicken/main.js
+++ static/chicken/main.js
@@ -72,6 +72,158 @@ let gifs = [
     // 'DALL·E-2024-01-15-21.58.45---A-chicken-on-a-raft-in-a-contemporary-conceptual-art-style.-The-image-challenges-traditional-perceptions-with-an-abstract-and-thought-provoking-approa.png',
     // 'DALL·E-2024-01-15-21.59.31---A-chicken-on-a-raft-in-a-Neo-Expressionist-style.-The-image-is-characterized-by-intense-emotion-and-raw-energy,-with-vigorous-brushwork-and-vivid-colo.png',
 ].map(src => "gifs/" + src);
+let songsAndGifs = [
+    {
+        "genre": "",
+        "song": 'club—chicken-on-a-raft.mp3',
+        "gif": 'dj.gif',
+    },
+    {
+        "genre": "",
+        "song": 'Metal-Madness.mp3',
+        "gif": 'metal.gif',
+    },
+    {
+        "genre": "",
+        "song": 'sad-piano—Reflections.mp3',
+        "gif": 'indie.gif',
+    },
+    {
+        "genre": "",
+        "song": 'folk—Hey-Oh,-Chicken-on-a-Raft.mp3',
+        "gif": 'DALL·E-2024-01-15-18.53.06---A-chicken-on-a-raft-in-a-warm-and-rustic-folk-style.-The-chicken-is-peaceful-and-serene,-depicted-with-natural-and-earthy-tones.-The-raft-is-made-of-n.png',
+    },
+    {
+        "genre": "",
+        "song": "sitar-infused-traditional-indian-The-Chicken's-Raft.mp3",
+        "gif": 'sunset.gif',
+    },
+    {
+        "genre": "",
+        "song": 'jazz-Chicken-on-a-Raft.mp3',
+        "gif": 'DALL·E-2024-01-15-18.48.02---A-jazz-album-cover-featuring-a-chicken-on-a-raft.-The-style-is-cool-and-sophisticated,-with-a-hint-of-vintage-charm.-The-chicken-is-stylized-in-a-slee.png',
+    },
+    {
+        "genre": "",
+        "song": "baroque-classical-choral-Chicken-on-a-Raft-(Baroque-Edition).mp3",
+        "gif": 'DALL·E-2024-01-15-18.47.57---An-opera-album-cover-with-a-chicken-on-a-raft.-The-style-is-grand-and-dramatic,-reflecting-the-theatrical-nature-of-opera.-The-chicken-is-depicted-in-.png',
+    },
+    {
+        "genre": "",
+        "song": 'sad-piano—Shadows-on-the-Keys.mp3',
+        "gif": 'DALL·E-2024-01-15-21.56.24---A-chicken-on-a-raft-in-a-contemporary-street-photography-style.-The-image-captures-a-candid,-spontaneous-moment-of-a-real-chicken-on-a-small-raft,-per.png',
+    },
+    {
+        "genre": "",
+        "song": 'club—Dance-All-Night.mp3',
+        "gif": 'DALL·E-2024-01-15-21.57.39---A-chicken-on-a-raft-in-a-Rococo-art-style.-The-image-is-light,-playful,-and-ornate,-with-delicate-colors-and-intricate-details.-The-chicken-is-adorned.png',
+    },
+    {
+        "genre": "",
+        "song": "funk-groovy--Funky-Chicken.mp3",
+        "gif": 'DALL·E-2024-01-15-21.56.27---A-chicken-on-a-raft-in-a-traditional-Chinese-ink-wash-painting-style.-The-image-is-elegant-and-minimalistic,-with-fluid-brushstrokes-and-a-focus-on-th.png',
+    },
+    {
+        "genre": "",
+        "song": 'pop-Chicken-on-a-Raft.mp3',
+        "gif": 'DALL·E-2024-01-15-21.56.14---A-chicken-on-a-raft-in-a-digital-pixel-art-style.-The-image-is-composed-of-vibrant-pixels,-creating-a-nostalgic-and-playful-scene.-The-chicken-is-depi.png',
+    },
+    {
+        "genre": "",
+        "song": 'country-Chicken-on-a-Raft-(2)-heavy-metal-hard-hitting-intense.mp3',
+        "gif": 'DALL·E-2024-01-15-21.46.48---A-chicken-on-a-raft-in-a-traditional-Japanese-ukiyo-e-woodblock-print-style.-The-image-features-classic-ukiyo-e-elements,-such-as-curving-lines-and-fl.png',
+    },
+    {
+        "genre": "",
+        "song": 'Skippers-Lament.mp3',
+        "gif": 'DALL·E-2024-01-15-21.57.43---A-chicken-on-a-raft-in-a-Pre-Raphaelite-art-style.-The-image-is-rich-in-symbolism-and-natural-detail,-with-an-emphasis-on-beauty-and-emotion.-The-chic.png',
+    },
+    {
+        "genre": "",
+        "song": "folk-traditional-chinese-enchanting-The-Adventure-of-the-Chicken-on-the-Raft.mp3",
+        "gif": 'DALL·E-2024-01-15-21.56.15---A-chicken-on-a-raft-in-a-street-art-graffiti-style.-The-image-is-vibrant-and-expressive,-with-bold-spray-paint-effects-and-urban-influences.-The-chick.png',
+    },
+    {
+        "genre": "",
+        "song": "traditional-indian-sitar-tabla-The-Rafting-Chicken.mp3",
+        "gif": 'DALL·E-2024-01-15-21.57.41---A-chicken-on-a-raft-in-a-Constructivist-art-style.-The-image-is-bold-and-geometric,-with-an-emphasis-on-abstract-forms-and-industrial-materials.-The-c.png',
+    },
+    {
+        "genre": "",
+        "song": 'chicken-hallelujah.mp3',
+        "gif": 'DALL·E-2024-01-15-21.56.18---A-chicken-on-a-raft-in-a-surrealistic-style-inspired-by-Salvador-Dali.-The-image-features-dream-like-elements-with-the-chicken-and-raft-melting-and-di.png',
+    },
+    {
+        "genre": "",
+        "song": 'Chicken-on-a-Raft-(Opera-Edition).mp3',
+        "gif": 'DALL·E-2024-01-15-18.42.49---An-album-cover-for-a-death-metal-song-featuring-a-chicken-on-a-raft.-The-style-is-dark-and-intense,-with-the-chicken-portrayed-in-a-fierce,-almost-men.png',
+    },
+    {
+        "genre": "",
+        "song": 'melodrama—chicken-sailors-ode.mp3',
+        "gif": 'DALL·E-2024-01-15-18.53.16---A-chicken-on-a-raft-in-a-grand-and-dramatic-opera-style.-The-chicken-is-depicted-in-a-theatrical-and-powerful-manner,-possibly-wearing-a-miniature-ope.png',
+    },
+    {
+        "genre": "",
+        "song": "funk-groovy--Funky-Chicken-(1).mp3",
+        "gif": 'DALL·E-2024-01-15-21.46.50---A-chicken-on-a-raft-in-a-minimalist,-modern-art-style.-The-image-is-composed-with-simple,-clean-lines-and-a-limited-color-palette.-The-chicken-is-styl.png',
+    },
+    {
+        "genre": "",
+        "song": 'poppy-chicken-symphony.mp3',
+        "gif": 'DALL·E-2024-01-15-21.46.52---A-chicken-on-a-raft-in-a-futuristic,-cyberpunk-style.-The-chicken-is-enhanced-with-neon-and-cybernetic-elements,-giving-it-a-bold,-edgy-look.-The-raft.png',
+    },
+    {
+        "genre": "",
+        "song": "isopolyphony-balkan-The-Chicken-on-the-Raft.mp3",
+        "gif": 'DALL·E-2024-01-15-21.46.54---A-chicken-on-a-raft-in-an-impressionist-painting-style.-The-image-features-soft-brushstrokes-and-a-focus-on-the-play-of-light-and-color.-The-chicken-i.png',
+    },
+    {
+        "genre": "",
+        "song": "lively-traditional-arabesque--Raftin'-Chicken.mp3",
+        "gif": 'DALL·E-2024-01-15-18.53.21---A-chicken-on-a-raft-in-a-cool-and-sophisticated-jazz-style.-The-chicken-is-stylized-in-a-sleek,-jazzy-manner,-possibly-with-elements-like-a-hat-or-a-s.png',
+    },
+    {
+        "genre": "",
+        "song": "medieval-choral-gregorianChanting-Chicken.mp3",
+        "gif": 'DALL·E-2024-01-15-21.51.14---A-chicken-on-a-raft-in-a-Gothic-art-style.-The-image-is-dark-and-moody,-with-dramatic-lighting-and-shadow-play.-The-chicken-is-depicted-in-a-mysteriou.png',
+    },
+    {
+        "genre": "",
+        "song": "traditional-maqam-folk-The-Majestic-Chicken.mp3",
+        "gif": 'DALL·E-2024-01-15-21.51.16---A-chicken-on-a-raft-in-a-pop-art-style.-The-image-features-bold-colors,-stark-outlines,-and-a-playful,-whimsical-approach.-The-chicken-is-depicted-in-.png',
+    },
+    {
+        "genre": "",
+        "song": "upbeat-traditional-chinese-folk-Chicken-on-a-Raft.mp3",
+        "gif": 'DALL·E-2024-01-15-21.51.18---A-chicken-on-a-raft-in-an-Art-Deco-style.-The-image-is-characterized-by-elegant,-geometric-shapes-and-streamlined-forms.-The-chicken-is-stylized-with-.png',
+    },
+    {
+        "genre": "",
+        "song": "-balkan-isopolyphony-traditionalThe-Raft-of-the-Chicken.mp3",
+        "gif": 'DALL·E-2024-01-15-21.51.22---A-chicken-on-a-raft-in-a-classic-Renaissance-painting-style.-The-image-features-a-realistic-and-detailed-depiction-of-the-chicken,-with-careful-attent.png',
+    },
+    {
+        "genre": "",
+        "song": "-choral-classical-Cluck-Cluck-Harmony.mp3",
+        "gif": 'DALL·E-2024-01-15-21.56.09---A-chicken-on-a-raft-in-a-Victorian-era-illustration-style.-The-image-has-a-classic,-vintage-feel,-with-detailed-line-work-and-a-monochromatic-color-sc.png',
+    },
+    {
+        "genre": "",
+        "song": "-solemn-gregorian-chants-reverentFeathered-Voyage.mp3",
+        "gif": 'DALL·E-2024-01-15-21.56.12---A-chicken-on-a-raft-in-a-Baroque-painting-style.-The-image-is-rich-and-dramatic,-with-deep-shadows-and-intense-lighting.-The-chicken-is-depicted-with-.png',
+    },
+    {
+        "genre": "",
+        "song": "-traditional-world-upbeat-Raftin'-Chicken.mp3",
+        "gif": 'DALL·E-2024-01-15-21.56.20---A-chicken-on-a-raft-in-a-Northern-Renaissance-style.-The-image-is-detailed-and-intricate,-with-an-emphasis-on-realism-and-fine-textures.-The-chicken-i.png',
+    },
+    {
+        "genre": "",
+        "song": "-upbeat-fusion-world-Raft-of-the-Rooster.mp3",
+        "gif": 'DALL·E-2024-01-15-21.56.22---A-chicken-on-a-raft-in-an-Expressionist-style.-The-image-is-characterized-by-intense,-vivid-colors-and-bold,-distorted-forms,-conveying-emotional-expe.png',
+    },
+]
 let currentSongIndex = 0;
 let currentGifIndex = 0;
 
